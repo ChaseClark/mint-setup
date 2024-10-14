09da81f4c58ac4489ea39fb38e198de4e8b31941
Chase Clark <chase_clark07@hotmail.com>
2024-10-14 09:55:47 -0400

    update mullvad for mint

--- install/desktop/app-mullvad.sh
+++ install/desktop/app-mullvad.sh
@@ -2,7 +2,7 @@
 sudo curl -fsSLo /usr/share/keyrings/mullvad-keyring.asc https://repository.mullvad.net/deb/mullvad-keyring.asc
 
 # Add the Mullvad repository server to apt
-echo "deb [signed-by=/usr/share/keyrings/mullvad-keyring.asc arch=$( dpkg --print-architecture )] https://repository.mullvad.net/deb/stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/mullvad.list
+echo "deb [signed-by=/usr/share/keyrings/mullvad-keyring.asc arch=$( dpkg --print-architecture )] https://repository.mullvad.net/deb/stable $(awk -F'=' '$1 == "UBUNTU_CODENAME" || $1 == "DEBIAN_CODENAME" { print $2 }' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/mullvad.list
 
 # Install the package
 sudo apt update
